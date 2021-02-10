d7134af170bff49649be3fcdc698fe5bc3f1d1d3
Steven Dahdah <steven.dahdah@mail.mcgill.ca>
2021-02-10 10:27:41 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='pykoop',
+    version='0.1.0',
+    description='Koopman operator identification library in Python',
+    long_description=readme,
+    author='Steven Dahdah',
+    author_email='steven.dahdah@mail.mcgill.ca',
+    url='https://bitbucket.org/decargroup/pykoop',
+    packages=setuptools.find_packages(exclude=('tests', 'demos')),
+    install_requires=['numpy', 'scipy', 'scikit-learn', 'picos'],
+    extra_require={'MOSEK solver': ['mosek']},
+)
