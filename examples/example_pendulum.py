e913e6a4090bae2b83f83a8c46c98d798fe67bf1
Steven Dahdah <steven.dahdah@mail.mcgill.ca>
2021-03-17 13:22:11 -0400

    Find numerically intractible example

--- examples/example_pendulum.py
+++ examples/example_pendulum.py
@@ -27,13 +27,15 @@ X = np.vstack((
 
 kp = koopman_pipeline.KoopmanPipeline(
     preprocessing=preprocessing.StandardScaler(),
-    delay=lifting_functions.Delay(n_delay_x=0, n_delay_u=0),
-    lifting_function=lifting_functions.PolynomialLiftingFn(order=1),
+    # preprocessing=preprocessing.MinMaxScaler((-1, 1)),
+    delay=lifting_functions.Delay(n_delay_x=2, n_delay_u=2),
+    lifting_function=lifting_functions.PolynomialLiftingFn(order=2),
     estimator=dmd.Edmd()
 )
 
 kp.fit(X.T, n_u=1)
 
+crash_index = None
 n_samp = kp.delay_.n_samples_needed_
 u_sim = np.reshape(u(sol.t), (1, -1))
 
@@ -45,8 +47,16 @@ for k in range(n_samp, X.shape[1]):
         X_sim[:, (k-n_samp):k],
         u_sim[:, (k-n_samp):k]
     ))
-    Xp = kp.predict(Xk.T).T
+    try:
+        Xp = kp.predict(Xk.T).T
+    except ValueError:
+        crash_index = k
+        break
     X_sim[:, [k]] = Xp[1:, [-1]]
+
+if crash_index is not None:
+    X_sim[:, crash_index:] = np.nan * np.ones((X_sim.shape[0], 1))
+    X_sim = np.ma.masked_invalid(X_sim)
 
 fig, ax = plt.subplots(3, 1)
 ax[0].plot(sol.t, sol.y[0, :])
@@ -54,4 +64,7 @@ ax[0].plot(sol.t, X_sim[0, :])
 ax[1].plot(sol.t, sol.y[1, :])
 ax[1].plot(sol.t, X_sim[1, :])
 ax[2].plot(sol.t, u(sol.t))
+
+ax[0].set_ylim((-10, 10))
+ax[1].set_ylim((-10, 10))
 plt.show()
