a84f857c35a10b50e44cc8d04946ed1bac0b6906
Steven Dahdah <steven.dahdah@mail.mcgill.ca>
2022-08-30 13:13:33 -0400

    Fully refactor lifting function unit tests

--- tests/test_lifting_functions.py
+++ tests/test_lifting_functions.py
@@ -288,16 +288,160 @@ import pykoop
             2,
             True,
         ),
+        (
+            pykoop.SkLearnLiftingFn(preprocessing.MaxAbsScaler()),
+            np.array([
+                [1., -1., 2.],
+                [2., 0., 0.],
+                [0., 1., -1.],
+            ]),
+            np.array([
+                [0.5, -1., 1.],
+                [1., 0., 0.],
+                [0., 1., -0.5],
+            ]),
+            0,
+            False,
+        ),
+        (
+            pykoop.SkLearnLiftingFn(preprocessing.StandardScaler()),
+            np.array([
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 1],
+                [0, 1, 1],
+            ]),
+            np.array([
+                [0, -1, -1],
+                [0, -1, -1],
+                [0, 1, 1],
+                [0, 1, 1],
+            ]),
+            0,
+            True,
+        ),
+        (
+            pykoop.SkLearnLiftingFn(
+                preprocessing.FunctionTransformer(
+                    func=np.log1p,
+                    inverse_func=lambda x: np.exp(x) - 1,
+                )),
+            np.array([
+                [0, 1],
+                [2, 3],
+            ]),
+            np.array([
+                [0., 0.69314718],
+                [1.09861229, 1.38629436],
+            ]),
+            0,
+            False,
+        ),
+        (
+            pykoop.BilinearInputLiftingFn(),
+            np.array([
+                [0, 1, 2, 3, 4, 5],
+                [1, 2, 3, 4, 5, 6],
+            ]).T,
+            np.array([
+                [0, 1, 2, 3, 4, 5],
+                [1, 2, 3, 4, 5, 6],
+            ]).T,
+            0,
+            False,
+        ),
+        (
+            pykoop.BilinearInputLiftingFn(),
+            np.array([
+                # States
+                [0, 1, 2, 3, 4, 5],
+                [1, 2, 3, 4, 5, 6],
+                [6, 5, 4, 3, 2, 1],
+                # Inputs
+                [5, 4, 3, 2, 1, 1],
+            ]).T,
+            np.array([
+                # x
+                [0, 1, 2, 3, 4, 5],
+                [1, 2, 3, 4, 5, 6],
+                [6, 5, 4, 3, 2, 1],
+                # u
+                [5, 4, 3, 2, 1, 1],
+                # x * u1
+                [0, 4, 6, 6, 4, 5],
+                [5, 8, 9, 8, 5, 6],
+                [30, 20, 12, 6, 2, 1],
+            ]).T,
+            1,
+            False,
+        ),
+        (
+            pykoop.BilinearInputLiftingFn(),
+            np.array([
+                # States
+                [0, 1, 2, 3, 4, 5],
+                [1, 2, 3, 4, 5, 6],
+                # Inputs
+                [6, 5, 4, 3, 2, 1],
+                [5, 4, 3, 2, 1, 1],
+            ]).T,
+            np.array([
+                # x
+                [0, 1, 2, 3, 4, 5],
+                [1, 2, 3, 4, 5, 6],
+                # u
+                [6, 5, 4, 3, 2, 1],
+                [5, 4, 3, 2, 1, 1],
+                # x * u1
+                [0, 5, 8, 9, 8, 5],
+                [6, 10, 12, 12, 10, 6],
+                # x * u2
+                [0, 4, 6, 6, 4, 5],
+                [5, 8, 9, 8, 5, 6],
+            ]).T,
+            2,
+            False,
+        ),
+        (
+            pykoop.BilinearInputLiftingFn(),
+            np.array([
+                # States
+                [0, 1, 2, 3, 4, 5],
+                # Inputs
+                [1, 2, 3, 4, 5, 6],
+                [6, 5, 4, 3, 2, 1],
+                [5, 4, 3, 2, 1, 1],
+            ]).T,
+            np.array([
+                # x
+                [0, 1, 2, 3, 4, 5],
+                # u
+                [1, 2, 3, 4, 5, 6],
+                [6, 5, 4, 3, 2, 1],
+                [5, 4, 3, 2, 1, 1],
+                # x * u1
+                [0, 2, 6, 12, 20, 30],
+                # x * u2
+                [0, 5, 8, 9, 8, 5],
+                # x * u3
+                [0, 4, 6, 6, 4, 5],
+            ]).T,
+            3,
+            False,
+        ),
     ],
 )
 class TestLiftingFnTransform:
+    """Test lifting function transform and inverse transform."""
 
     def test_transform(self, lf, X, Xt_exp, n_inputs, episode_feature):
+        """Test lifting function transform."""
         lf.fit(X, n_inputs=n_inputs, episode_feature=episode_feature)
         Xt = lf.transform(X)
         np.testing.assert_allclose(Xt_exp, Xt)
 
     def test_inverse_transform(self, lf, X, Xt_exp, n_inputs, episode_feature):
+        """Test lifting function inverse transform."""
         lf.fit(X, n_inputs=n_inputs, episode_feature=episode_feature)
         Xt = lf.transform(X)
         Xt_inv = lf.inverse_transform(Xt)
@@ -630,13 +774,16 @@ class TestLiftingFnTransform:
     ],
 )
 class TestDelayLiftingFnTransform:
+    """Test :class:`DelayLiftingFn` transform and inverse transform."""
 
     def test_transform(self, lf, X, Xt_exp, n_inputs, episode_feature):
+        """Test :class:`DelayLiftingFn` transform."""
         lf.fit(X, n_inputs=n_inputs, episode_feature=episode_feature)
         Xt = lf.transform(X)
         np.testing.assert_allclose(Xt_exp, Xt)
 
     def test_inverse_transform(self, lf, X, Xt_exp, n_inputs, episode_feature):
+        """Test :class:`DelayLiftingFn` inverse transform."""
         lf.fit(X, n_inputs=n_inputs, episode_feature=episode_feature)
         Xt = lf.transform(X)
         Xt_inv = lf.inverse_transform(Xt)
