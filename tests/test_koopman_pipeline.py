4c607058e9cb132b4bb874554353896e2f5778d5
Steven Dahdah <steven.dahdah@mail.mcgill.ca>
2022-09-01 15:06:44 -0400

    Refactor episode manipulation tests

--- tests/test_koopman_pipeline.py
+++ tests/test_koopman_pipeline.py
@@ -8,63 +8,347 @@ from sklearn import preprocessing
 import pykoop
 
 
-@pytest.mark.parametrize('kp', [
-    pykoop.KoopmanPipeline(
-        lifting_functions=[
-            ('dl', pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=1))
-        ],
-        regressor=pykoop.Edmd(),
-    ),
-    pykoop.KoopmanPipeline(
-        lifting_functions=[
-            ('dla', pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=2)),
-            ('dlb', pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=2)),
-        ],
-        regressor=pykoop.Edmd(),
-    ),
-    pykoop.KoopmanPipeline(
-        lifting_functions=[
-            ('dla', pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=1)),
-            ('ply', pykoop.PolynomialLiftingFn(order=2)),
-            ('dlb', pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=2)),
-        ],
-        regressor=pykoop.Edmd(),
-    ),
-    pykoop.KoopmanPipeline(
-        lifting_functions=[
+class TestEpisodeManipulation:
+    """Test episode manipulation, including shifting and splitting."""
+
+    @pytest.mark.parametrize(
+        'X, X_unsh_exp, X_sh_exp, n_inputs, episode_feature',
+        [
+            # Test without input or episode feature
             (
-                'sp',
-                pykoop.SplitPipeline(
-                    lifting_functions_state=[
-                        ('pl', pykoop.PolynomialLiftingFn(order=2)),
-                    ],
-                    lifting_functions_input=None,
-                ),
+                np.array([
+                    [1, 2, 3, 4, 5],
+                    [-1, -2, -3, -4, -5],
+                ]).T,
+                np.array([
+                    [1, 2, 3, 4],
+                    [-1, -2, -3, -4],
+                ]).T,
+                np.array([
+                    [2, 3, 4, 5],
+                    [-2, -3, -4, -5],
+                ]).T,
+                0,
+                False,
             ),
-            ('dl', pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=2)),
-        ],
-        regressor=pykoop.Edmd(),
-    ),
-    pykoop.KoopmanPipeline(
-        lifting_functions=[
-            ('dla', pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=1)),
+            # Test with input, without episode feature
             (
-                'sp',
-                pykoop.SplitPipeline(
-                    lifting_functions_state=[
-                        ('pla', pykoop.PolynomialLiftingFn(order=2)),
-                        ('plb', pykoop.PolynomialLiftingFn(order=2)),
-                    ],
-                    lifting_functions_input=[
-                        ('plc', pykoop.PolynomialLiftingFn(order=2)),
-                    ],
-                ),
+                np.array([
+                    [1, 2, 3, 4, 5],
+                    [-1, -2, -3, -4, -5],
+                ]).T,
+                np.array([
+                    [1, 2, 3, 4],
+                    [-1, -2, -3, -4],
+                ]).T,
+                np.array([
+                    [2, 3, 4, 5],
+                ]).T,
+                1,
+                False,
             ),
-            ('dlb', pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=1)),
+            # Test without input, with episode feature
+            (
+                np.array([
+                    [0, 0, 0, 1, 1],
+                    [1, 2, 3, 4, 5],
+                    [-1, -2, -3, -4, -5],
+                ]).T,
+                np.array([
+                    [0, 0, 1],
+                    [1, 2, 4],
+                    [-1, -2, -4],
+                ]).T,
+                np.array([
+                    [0, 0, 1],
+                    [2, 3, 5],
+                    [-2, -3, -5],
+                ]).T,
+                0,
+                True,
+            ),
+            # Test with input and episode feature
+            (
+                np.array([
+                    [0, 0, 1, 1, 1],
+                    [1, 2, 3, 4, 5],
+                    [-1, -2, -3, -4, -5],
+                ]).T,
+                np.array([
+                    [0, 1, 1],
+                    [1, 3, 4],
+                    [-1, -3, -4],
+                ]).T,
+                np.array([
+                    [0, 1, 1],
+                    [2, 4, 5],
+                ]).T,
+                1,
+                True,
+            ),
         ],
-        regressor=pykoop.Edmd(),
-    ),
-])
+    )
+    def test_shift_episodes(
+        self,
+        X,
+        X_unsh_exp,
+        X_sh_exp,
+        n_inputs,
+        episode_feature,
+    ):
+        """Test :func:`shift_episodes`.
+
+        .. todo:: Break up multiple asserts.
+        """
+        X_unsh, X_sh = pykoop.shift_episodes(X, n_inputs, episode_feature)
+        np.testing.assert_allclose(X_unsh, X_unsh_exp)
+        np.testing.assert_allclose(X_sh, X_sh_exp)
+
+    @pytest.mark.parametrize(
+        'X, min_samples, n_inputs, episode_feature, ic_exp',
+        [
+            (
+                np.array([
+                    [1, 2, 3, 4],
+                    [4, 5, 6, 7],
+                ]).T,
+                1,
+                0,
+                False,
+                np.array([
+                    [1],
+                    [4],
+                ]).T,
+            ),
+            (
+                np.array([
+                    [1, 2, 3, 4],
+                    [4, 5, 6, 7],
+                ]).T,
+                2,
+                0,
+                False,
+                np.array([
+                    [1, 2],
+                    [4, 5],
+                ]).T,
+            ),
+            (
+                np.array([
+                    [1, 2, 3, 4],
+                    [4, 5, 6, 7],
+                    [5, 5, 5, 5],
+                ]).T,
+                1,
+                1,
+                False,
+                np.array([
+                    [1],
+                    [4],
+                ]).T,
+            ),
+            (
+                np.array([
+                    [0, 0, 1, 1],
+                    [1, 2, 3, 4],
+                    [4, 5, 6, 7],
+                ]).T,
+                1,
+                0,
+                True,
+                np.array([
+                    [0, 1],
+                    [1, 3],
+                    [4, 6],
+                ]).T,
+            ),
+            (
+                np.array([
+                    [0, 0, 1, 1],
+                    [1, 2, 3, 4],
+                    [4, 5, 6, 7],
+                    [9, 9, 9, 9],
+                ]).T,
+                1,
+                1,
+                True,
+                np.array([
+                    [0, 1],
+                    [1, 3],
+                    [4, 6],
+                ]).T,
+            ),
+            (
+                np.array([
+                    [0, 0, 0, 1, 1, 1],
+                    [1, 2, 2, 3, 4, 5],
+                    [4, 5, 5, 6, 7, 6],
+                    [9, 9, 9, 9, 9, 6],
+                ]).T,
+                2,
+                1,
+                True,
+                np.array([
+                    [0, 0, 1, 1],
+                    [1, 2, 3, 4],
+                    [4, 5, 6, 7],
+                ]).T,
+            ),
+        ],
+    )
+    def test_extract_initial_conditions(
+        self,
+        X,
+        min_samples,
+        n_inputs,
+        episode_feature,
+        ic_exp,
+    ):
+        """Test :func:`extract_initial_conditions`."""
+        ic = pykoop.extract_initial_conditions(
+            X,
+            min_samples,
+            n_inputs,
+            episode_feature,
+        )
+        np.testing.assert_allclose(ic, ic_exp)
+
+    @pytest.mark.parametrize(
+        'X, n_inputs, episode_feature, u_exp',
+        [
+            (
+                np.array([
+                    [1, 2, 3, 4],
+                    [6, 7, 8, 9],
+                ]).T,
+                1,
+                False,
+                np.array([
+                    [6, 7, 8, 9],
+                ]).T,
+            ),
+            (
+                np.array([
+                    [1, 2, 3, 4],
+                    [6, 7, 8, 9],
+                ]).T,
+                0,
+                False,
+                np.array([]).reshape((0, 4)).T,
+            ),
+            (
+                np.array([
+                    [0, 0, 1, 1],
+                    [1, 2, 3, 4],
+                    [6, 7, 8, 9],
+                ]).T,
+                1,
+                True,
+                np.array([
+                    [0, 0, 1, 1],
+                    [6, 7, 8, 9],
+                ]).T,
+            ),
+        ],
+    )
+    def test_extract_input(self, X, n_inputs, episode_feature, u_exp):
+        """Test :func:`extract_input`."""
+        u = pykoop.extract_input(X, n_inputs, episode_feature)
+        np.testing.assert_allclose(u, u_exp)
+
+    def test_strip_initial_conditons(self):
+        """Test :func:`strip_initial_conditions`."""
+        X1 = np.array([
+            [0, 0, 1, 1, 1, 2, 2, 2],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            [2, 3, 4, 5, 6, 7, 8, 9],
+        ]).T
+        X2 = np.array([
+            [0, 0, 1, 1, 1, 2, 2, 2],
+            [-1, 2, -1, 4, 5, -1, 7, 8],
+            [-1, 3, -1, 5, 6, -1, 8, 9],
+        ]).T
+        X1s = pykoop.strip_initial_conditions(
+            X1,
+            min_samples=1,
+            episode_feature=True,
+        )
+        X2s = pykoop.strip_initial_conditions(
+            X2,
+            min_samples=1,
+            episode_feature=True,
+        )
+        np.testing.assert_allclose(X1s, X2s)
+
+
+@pytest.mark.parametrize(
+    'kp',
+    [
+        pykoop.KoopmanPipeline(
+            lifting_functions=[
+                ('dl', pykoop.DelayLiftingFn(n_delays_state=1,
+                                             n_delays_input=1))
+            ],
+            regressor=pykoop.Edmd(),
+        ),
+        pykoop.KoopmanPipeline(
+            lifting_functions=[
+                ('dla',
+                 pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=2)),
+                ('dlb',
+                 pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=2)),
+            ],
+            regressor=pykoop.Edmd(),
+        ),
+        pykoop.KoopmanPipeline(
+            lifting_functions=[
+                ('dla',
+                 pykoop.DelayLiftingFn(n_delays_state=2, n_delays_input=1)),
+                ('ply', pykoop.PolynomialLiftingFn(order=2)),
+                ('dlb',
+                 pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=2)),
+            ],
+            regressor=pykoop.Edmd(),
+        ),
+        pykoop.KoopmanPipeline(
+            lifting_functions=[
+                (
+                    'sp',
+                    pykoop.SplitPipeline(
+                        lifting_functions_state=[
+                            ('pl', pykoop.PolynomialLiftingFn(order=2)),
+                        ],
+                        lifting_functions_input=None,
+                    ),
+                ),
+                ('dl', pykoop.DelayLiftingFn(n_delays_state=2,
+                                             n_delays_input=2)),
+            ],
+            regressor=pykoop.Edmd(),
+        ),
+        pykoop.KoopmanPipeline(
+            lifting_functions=[
+                ('dla',
+                 pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=1)),
+                (
+                    'sp',
+                    pykoop.SplitPipeline(
+                        lifting_functions_state=[
+                            ('pla', pykoop.PolynomialLiftingFn(order=2)),
+                            ('plb', pykoop.PolynomialLiftingFn(order=2)),
+                        ],
+                        lifting_functions_input=[
+                            ('plc', pykoop.PolynomialLiftingFn(order=2)),
+                        ],
+                    ),
+                ),
+                ('dlb',
+                 pykoop.DelayLiftingFn(n_delays_state=1, n_delays_input=1)),
+            ],
+            regressor=pykoop.Edmd(),
+        ),
+    ],
+)
 class TestPrediction:
     """Test fit Koopman pipeline prediction."""
 
@@ -249,8 +533,7 @@ class TestSplitCombineEpisodes:
     def test_split_episodes(self, X, episodes, episode_feature):
         """Test :func:`split_episodes`.
 
-        .. todo::
-            Break up multiple asserts.
+        .. todo:: Break up multiple asserts.
         """
         # Split episodes
         episodes_actual = pykoop.split_episodes(
@@ -599,15 +882,18 @@ class TestSplitPipeline:
         np.testing.assert_allclose(Xi, X)
 
 
-@pytest.mark.parametrize('lf', [
-    pykoop.PolynomialLiftingFn(order=2),
-    pykoop.KoopmanPipeline(
-        lifting_functions=[
-            ('p', pykoop.PolynomialLiftingFn(order=2)),
-        ],
-        regressor=pykoop.Edmd(),
-    )
-])
+@pytest.mark.parametrize(
+    'lf',
+    [
+        pykoop.PolynomialLiftingFn(order=2),
+        pykoop.KoopmanPipeline(
+            lifting_functions=[
+                ('p', pykoop.PolynomialLiftingFn(order=2)),
+            ],
+            regressor=pykoop.Edmd(),
+        )
+    ],
+)
 class TestLiftRetract:
     """Test estimators with :class:`_LiftRetractMixin`.
 
@@ -618,8 +904,7 @@ class TestLiftRetract:
     Xt : np.ndarray
         Transformed data matrix.
 
-    .. todo::
-        Break up multiple asserts.
+    .. todo:: Break up multiple asserts.
     """
 
     X = np.array([
